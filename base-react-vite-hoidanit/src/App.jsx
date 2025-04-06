69df3ee6372775deec8f795d0580bc778e82c18d
QuocToan1604 <huynhquoctoan1642002@gmail.com>
2025-04-06 19:49:38 +0700

    video 34

--- base-react-vite-hoidanit/src/App.jsx
+++ base-react-vite-hoidanit/src/App.jsx
@@ -3,11 +3,22 @@ import TodoNew from './components/todo/TodoNew'
 import TodoData from './components/todo/TodoData'
 import reactLogo from "./assets/react.svg"
 const App = () => {
+  const hoidanit = "Quoc Toan";
+  const age = 25;
+  const data = {
+    address: "Hanoi",
+    country: "VietNam"
+  }
+
   return (
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
       <TodoNew />
-      <TodoData />
+      <TodoData
+        name={hoidanit}
+        age={age}
+        data={data}
+      />
       <div className='todo-image'>
         <img className='logo' src={reactLogo} />
       </div>
--- base-react-vite-hoidanit/src/components/todo/TodoData.jsx
+++ base-react-vite-hoidanit/src/components/todo/TodoData.jsx
@@ -1,6 +1,10 @@
-const TodoData = () => {
+const TodoData = (props) => {
+    const { name } = props;
+    console.log("props", props)
     return (
         <div className='todo-data'>
+            <div>My name is {props.name}</div>
+            <div>My name is {name}</div>
             <div>Learning React</div>
             <div>Watching Youtube</div>
         </div>
