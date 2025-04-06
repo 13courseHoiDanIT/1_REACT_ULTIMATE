be043afa8023e13f646f8d69327a495ea2b7d7d5
QuocToan1604 <huynhquoctoan1642002@gmail.com>
2025-04-06 17:06:35 +0700

    video 31

--- base-react-vite-hoidanit/src/components/learn/MyComponent.jsx
+++ base-react-vite-hoidanit/src/components/learn/MyComponent.jsx
@@ -3,9 +3,18 @@
 
 import './style.css'
 const MyComponent = () => {
+    // const hoidanit = "Eric"; //string
+    // const hoidanit = 25; //string
+    // const hoidanit = true; //string
+    const hoidanit = {
+        name: "Eric",
+        age: 25
+    }
     return (
         <>
-            <div > Eric & hoidanIT</div>
+            <div > {hoidanit.name} & hoidanIT</div>
+            <div > {JSON.stringify(hoidanit)} & hoidanIT</div>
+            <div> {console.log("eric")}</div>
             <div className="child">child</div>
         </>
     );
