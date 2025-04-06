d198ea5926b0edde34a1cbf2bae9503e4e6ea555
QuocToan1604 <huynhquoctoan1642002@gmail.com>
2025-04-06 20:00:33 +0700

    video 35

--- base-react-vite-hoidanit/src/components/todo/TodoNew.jsx
+++ base-react-vite-hoidanit/src/components/todo/TodoNew.jsx
@@ -1,4 +1,7 @@
-const TodoNew = () => {
+const TodoNew = (props) => {
+    console.log("check props", props)
+    const { addNewTodo } = props;
+    addNewTodo()
     return (
         <div className="todo-new">
             <input type="text" placeholder="Enter your task" />
