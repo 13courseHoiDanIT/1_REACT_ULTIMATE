ac703d2dcd810c8a9e7293356f335857e95efcec
QuocToan1604 <huynhquoctoan1642002@gmail.com>
2025-07-10 02:32:45 +0700

    video 49

--- base-react-vite-hoidanit/src/components/layout/header.jsx
+++ base-react-vite-hoidanit/src/components/layout/header.jsx
@@ -1,11 +1,12 @@
+import { Link } from 'react-router-dom';
 import './header.scss'
 
 const Header = () => {
     return (
         <ul>
-            <li><a class="active" href="#home">Home</a></li>
-            <li><a href="/users">User</a></li>
-            <li><a href="/product">Product</a></li>
+            <li><Link class="active" to="#home">Home</Link></li>
+            <li><Link to="/users">User</Link></li>
+            <li><Link to="/product">Product</Link></li>
         </ul>
     )
 }
