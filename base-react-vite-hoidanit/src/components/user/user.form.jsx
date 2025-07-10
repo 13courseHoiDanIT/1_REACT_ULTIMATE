2e3e7f918704c758441cca52e0990ad1da10ec6a
QuocToan1604 <huynhquoctoan1642002@gmail.com>
2025-07-10 12:46:24 +0700

    video 67

--- base-react-vite-hoidanit/src/components/user/user.form.jsx
+++ base-react-vite-hoidanit/src/components/user/user.form.jsx
@@ -1,28 +1,48 @@
 import { Input } from "antd";
 import { Button } from "antd";
+import { useState } from "react";
 
 const UserForm = () => {
+    const [fullName, setFullName] = useState("quoc");
+    const [email, setEmail] = useState("")
+    const [password, setPasswpord] = useState("")
+    const [phoneNumber, setPhoneNumber] = useState("")
+    const handleClickBtn = () => {
+        console.log("c", { fullName, email, password, phoneNumber })
+    }
     return (
         <div className="user-form" style={{ margin: "20px 0" }}>
             <div style={{ display: "flex", gap: "15px", flexDirection: "column" }}>
                 <div>
                     <span>FullName</span>
-                    <Input />
+                    <Input
+                        value={fullName}
+                        onChange={(event) => { setFullName(event.target.value) }}
+                    />
                 </div>
                 <div>
                     <span>Email</span>
-                    <Input />
+                    <Input
+                        value={email}
+                        onChange={(event) => { setEmail(event.target.value) }}
+                    />
                 </div>
                 <div>
                     <span>Password</span>
-                    <Input.Password />
+                    <Input.Password
+                        value={password}
+                        onChange={(event) => { setPasswpord(event.target.value) }}
+                    />
                 </div>
                 <div>
                     <span>Phone number</span>
-                    <Input />
+                    <Input
+                        value={phoneNumber}
+                        onChange={(event) => { setPhoneNumber(event.target.value) }}
+                    />
                 </div>
                 <div>
-                    <Button type="primary">Create User</Button >
+                    <Button type="primary" onClick={handleClickBtn}>Create User</Button >
                 </div>
             </div>
         </div>
